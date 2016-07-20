b789b7b5eb7badde2c95f45a30ea76c604aefcfc
n2omatt <n2o.matt@gmail.com>
2016-07-19 21:59:03 -0300

    Add the build scripts for ~n2omatt

---
+++ ~n2omatt/_build_stuff/certification_scripts/update_index_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+index_source_filename = "../../_index_original.html";
+certs_source_filename = "../../certs.html";
+index_target_filename = "../../index.html";
+
+
+index_source = file(index_source_filename, "r").readlines();
+certs_source = file(certs_source_filename, "r").readlines();
+
+index_target = file(index_target_filename, "w");
+
+for index_source_line in index_source:
+    print index_source_line;
+    if(index_source_line.replace("\n", "").lstrip(" ").rstrip(" ") != "__CERTS_PLACEHOLDER__"):
+        index_target.write(index_source_line);
+    else:
+        for certs_source_line in certs_source:
+            index_target.write(certs_source_line);
+
+index_target.close();

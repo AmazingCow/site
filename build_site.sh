9cfd748d9622ba0821a5914525ec00a96a0982c1
n2omatt <n2omatt@amazingcow.com>
2017-10-15 14:41:20 -0400

    - Fix some errors.

--- build_site.sh
+++ build_site.sh
@@ -9,37 +9,38 @@ rm    -rf _output;
 mkdir -p  _output;
 
 # ## Repos are needed for other stuff...
-# ./scripts/download_repos.py
-# ./scripts/update_repos.py
+./scripts/download_repos.py;
+./scripts/update_repos.py;
 
 ################################################################################
 ## Index page.                                                                ##
 ################################################################################
-./scripts/build_index.py ./templates/index/index.template > _output/index.html
+./scripts/build_index.py ./templates/index/index.template > _output/index.html;
 
-# ################################################################################
-# ## Docs                                                                       ##
-# ################################################################################
-# ./scripts/generate_docs.py > ./templates/docs.list;
-# ./scripts/build_index.py ./templates/docs.template > ./docs/index.html
+################################################################################
+## Docs                                                                       ##
+################################################################################
+mkdir -p _output/docs;
+./scripts/generate_docs.py > ./templates/docs/docs.list;
+./scripts/build_index.py ./templates/docs/index.template > _output/docs/index.html;
 
 ################################################################################
 ## Floss                                                                      ##
 ################################################################################
-mkdir -p _output/floss/
-./scripts/build_index.py ./templates/floss/index.template > _output/floss/index.html
+mkdir -p _output/floss;
+./scripts/build_index.py ./templates/floss/index.template > _output/floss/index.html;
 
 ################################################################################
 ## Contact                                                                    ##
 ################################################################################
-mkdir -p _output/contact/
-./scripts/build_index.py ./templates/contact/index.template > _output/contact/index.html
+mkdir -p _output/contact;
+./scripts/build_index.py ./templates/contact/index.template > _output/contact/index.html;
 
 
-# ################################################################################
-# ## Images                                                                     ##
-# ################################################################################
-# cp -R img _output/img
+################################################################################
+## Images                                                                     ##
+################################################################################
+cp -R img _output/img
 
 
 # ################################################################################
